c45775eeeb06da3af5503fa70385a12465f76840
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-11-20 17:58:27 +0600

    Finished the 'Task 2' of the 'Module 2' and wrote tests for it

---
+++ Module2.MSTests/RecursiveSearchTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Module2.MSTests
+{
+    [TestClass]
+    public class RecursiveSearchTests
+    {
+        [DataTestMethod]
+        [DataRow(new int[5] { 5, 3, 4, 6, 2 }, 6)]
+        [DataRow(new int[10] { 1, 5, 2, 3, 4, 7, 8, 1, 5, 9 }, 9)]
+        [DataRow(new int[10] { 10, 5, 10, 4, 3, 7, 8, 1, 5, 9 }, 10)]
+        [DataRow(new int[10] { 1, 5, 5, 4, 3, 7, 8, 1, 5, 10 }, 10)]
+        [DataRow(new int[1] { 0 }, 0)]
+        [DataRow(new int[3] { 1, 1, 1 }, 1)]
+        public void RecursiveSearch_ReturnsMaxNumber(int[] arr, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Task2.RecursiveSearch(arr));
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { })]
+        public void RecursiveSearch_ThrowsArgumentExceptionIfArrLengthIsZero(int[] arr)
+        {
+            Assert.ThrowsException<ArgumentException>(() => Task2.RecursiveSearch(arr));
+        }
+    }
+}
--- Module2/Task2.cs
+++ Module2/Task2.cs
@@ -1,13 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Module2
 {
-    public class Task2
+    public static class Task2
     {
+        public static int RecursiveSearch(int[] arr)
+        {
+            if (arr.Length == 0)
+            {
+                throw new ArgumentException("Array is empty", nameof(arr));
+            }
 
+            int id = 0;
+            int current = arr[id++];
+            return IsMax(arr, id, current);
+        }
+
+        private static int IsMax(int[] arr, int id, int current)
+        {
+            if (id == arr.Length)
+            {
+                return current;
+            }
+            else if (current >= arr[id])
+            {
+                id++;
+                return IsMax(arr, id, current);
+            }
+            else
+            {
+                current = arr[id];
+                id++;
+                return IsMax(arr, id, current);
+            }
+        }
     }
 }
