74482241bd96feba4972a802a4940b74db42914d
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2021-11-23 18:42:42 +0600

    Finished the 'Task 6' of the 'Module 2' and wrote tests for it

---
+++ Module2.MSTests/FilterDigitTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Module2.MSTests
+{
+    [TestClass]
+    public class FilterDigitTests
+    {
+        private const string _provider = @"Microsoft.VisualStudio.TestTools.DataSource.XML";
+        private const string _connectionString = "ExternalFilesForDDT/FilterDigitTestData.xml";
+        private const string _tableName = "Data";
+        public TestContext TestContext { get; set; }
+
+        [DataSource(_provider, _connectionString, _tableName, DataAccessMethod.Sequential)]
+        [TestMethod()]
+        public void FilterDigit_ReturnsResultArr()
+        {
+            string inputArrString = TestContext.DataRow["inputArray"].ToString();
+            int digit = Convert.ToInt32(TestContext.DataRow["digit"]);
+            string expectedString = TestContext.DataRow["outputArray"].ToString();
+            int[] inputArr = FromStringArray(inputArrString);
+            int[] expected = FromStringArray(expectedString);
+            int[] actual = Task6.FilterDigit(inputArr, digit);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        private int[] FromStringArray(string stringArr)
+        {
+            if (stringArr.Length == 0)
+            {
+                return new int [] { };
+            }
+
+            string[] splittedString = stringArr.Split(' ');
+            int[] resultArr = new int[splittedString.Length];
+            for (int i = 0; i < splittedString.Length; i++)
+            {
+                resultArr[i] = Convert.ToInt32(splittedString[i]);
+            }
+
+            return resultArr;
+        }
+    }
+}
