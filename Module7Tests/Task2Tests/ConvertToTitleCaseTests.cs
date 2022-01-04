d3d66a4d662ccb5e809d01ceeacae7be13f4fcc6
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2022-01-04 22:19:02 +0600

    Finished the 'Task 2' of the 'Module 7'

---
+++ Module7Tests/Task2Tests/ConvertToTitleCaseTests.cs
@@ -0,0 +1,22 @@
+﻿using Module7.Task2;
+using NUnit.Framework;
+using System;
+
+namespace Module7Tests.Task2Tests
+{
+    public class ConvertToTitleCaseTests
+    {
+        [TestCase("a clash of KINGS", "a of", "A Clash of Kings")]
+        [TestCase("THE WIND IN THE WILLOWS", "The In", "The Wind in the Willows")]
+        [TestCase("the quick brown fox", null, "The Quick Brown Fox")]
+        public void ConvertStringToTitleCase_ReturnsResult(string inputString, string minorWords, string expected)
+        {
+            var result = ConvertToTitleCase.ConvertStringToTitleCase(inputString, minorWords);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(null)]
+        public void ConvertStringToTitleCase_NullInputString_ThrowsArgumentNullException(string inputString)
+            => Assert.Throws<ArgumentNullException>(() => ConvertToTitleCase.ConvertStringToTitleCase(inputString));
+    }
+}
