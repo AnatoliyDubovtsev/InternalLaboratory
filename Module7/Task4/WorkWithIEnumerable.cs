04a46f1f99ef8d5b01e36f64844ef674c6f6d17f
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2022-01-05 23:08:59 +0600

    Finished the 'Task 4' of the 'Module 7'

---
+++ Module7/Task4/WorkWithIEnumerable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Module7.Task4
+{
+    public static class WorkWithIEnumerable
+    {
+        public static List<T> UniqueInOrder<T>(IEnumerable<T> collection)
+        {
+            List<T> inputCollection = collection.ToList();
+            for (int i = 0; i < inputCollection.Count; i++)
+            {
+                for (int j = i + 1; j < inputCollection.Count; j++)
+                {
+                    if (inputCollection[i].Equals(inputCollection[j]))
+                    {
+                        inputCollection.RemoveAt(i--);
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return inputCollection;
+        }
+    }
+}
