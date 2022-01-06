5ddafd856e39e2e6d4c49bc8a34b3d76aff6ffa3
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2022-01-06 16:56:37 +0600

    Finished the 'Task 6' of the 'Module 7'

---
+++ Module7/Task6/WorkWithBigNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace Module7.Task6
+{
+    public static class WorkWithBigNumbers
+    {
+        public static string Sum(string left, string right)
+        {
+            if (string.IsNullOrEmpty(left))
+            {
+                throw new ArgumentNullException(nameof(left), "Left string is null or empty");
+            }
+            else if (string.IsNullOrEmpty(right))
+            {
+                throw new ArgumentNullException(nameof(right), "Right string is null or empty");
+            }
+
+            BigInteger result;
+            try
+            {
+                BigInteger leftNumber = BigInteger.Parse(left);
+                BigInteger rightNumber = BigInteger.Parse(right);
+                result = leftNumber + rightNumber;
+            }
+            catch(FormatException ex)
+            {
+                return ex.Message;
+            }
+
+            return result.ToString();
+        }
+    }
+}
