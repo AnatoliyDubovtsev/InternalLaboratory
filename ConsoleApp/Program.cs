be55fa152ad5df110092d26a05e5a42da30abca6
AnatoliyDubovtsev <anatoliy.dubovtsev01@gmail.com>
2022-01-31 17:44:51 +0600

    Updated versions of the projects

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Module10.Task2;
+using Module2;
 using Module3.Task2;
 using Module4.Task2;
 using Module6.Task2;
@@ -17,13 +18,13 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             // Module 2, Task 5
-            /*Console.Write("Enter the number -> ");
+            Console.Write("Enter the number -> ");
             int number = int.Parse(Console.ReadLine());
             int newNumber = Task5.FindNextBiggerNumber(number, out double elapsedMilliseconds);
-            Console.WriteLine($"New number: {newNumber}, Elapsed time: {elapsedMilliseconds} ms");*/
+            Console.WriteLine($"New number: {newNumber}, Elapsed time: {elapsedMilliseconds} ms");
 
             // Module 3, Task 2
-            /*int[,] matrix = new int[,]
+            int[,] matrix = new int[,]
             {
                 { 1, 2, 3 },
                 { 3, 4, 5 },
@@ -33,10 +34,10 @@ namespace ConsoleApp
             ISortingStrategy sortingStrategy = SortingConfiguration.ChooseSortingStrategy();
             SortMatrix sortMatrix = new(sortingStrategy);
             SortingConfiguration.ChooseTypeOfSorting(out bool isAscendingSorting);
-            sortMatrix.Sorting(matrix, isAscendingSorting);*/
+            sortMatrix.Sorting(matrix, isAscendingSorting);
 
             // Module 4, Task 2
-            /*Console.WriteLine("Euclidean algorithm");
+            Console.WriteLine("Euclidean algorithm");
             IGcdAlgorithm gcdAlgorithm = new EuclideanAlgorithm();
             int gcdEuclideanAlgorithm = gcdAlgorithm.FindGcd(out double elapsedMillisecondsEuclideanAlgorithm, new int[]
                 { 1375800, 9876090, 3859650, 456000, 756890, 957000 });
@@ -45,25 +46,25 @@ namespace ConsoleApp
             gcdAlgorithm = new BinaryEuclideanAlgorithm();
             int gcdBinaryEuclideanAlgorithm = gcdAlgorithm.FindGcd(out double elapsedMillisecondsBinaryEuclideanAlgorithm, new int[]
                 { 1375800, 9876090, 3859650, 456000, 756890, 957000 });
-            Console.WriteLine($"GCD: {gcdBinaryEuclideanAlgorithm}, Time: {elapsedMillisecondsBinaryEuclideanAlgorithm}");*/
+            Console.WriteLine($"GCD: {gcdBinaryEuclideanAlgorithm}, Time: {elapsedMillisecondsBinaryEuclideanAlgorithm}");
 
             // Module 6, Task 2
 
-            /*List<Shape> shapes = new List<Shape>
+            List<Shape> shapes = new List<Shape>
             {
                 new Circle("Circle", 5),
                 new Square("Square", 5),
-                new Triangle("Triangle", 10, 5),
+                new Triangle("Triangle", 10, 5, 5),
                 new Rectangle("Rectangle", 10, 5)
             };
             IVisitor visitor = new ShapesAreaVisitor();
             foreach(var shape in shapes)
             {
                 Console.WriteLine(shape.Area(visitor));
-            }*/
+            }
 
             // Module 6, Task 3
-            /*Console.WriteLine("Input polynomial. Use whitespace to divide coefficients. " + Environment.NewLine + "Example: a * x^0 + b * x^1 + c * x^2 + ... + d * x^n");
+            Console.WriteLine("Input polynomial. Use whitespace to divide coefficients. " + Environment.NewLine + "Example: a * x^0 + b * x^1 + c * x^2 + ... + d * x^n");
             //string input1 = Console.ReadLine();
             string input1 = "1 2 3 4 5 6 7 8";
             int[] array1 = Polynomial.ConvertToArray(input1);
@@ -75,20 +76,20 @@ namespace ConsoleApp
             Polynomial plus = polynomial1 + polynomial2;
             Console.WriteLine("1: " + polynomial1.ToString());
             Console.WriteLine("2: " + polynomial2.ToString());
-            Console.WriteLine("1 + 2: " + plus.ToString());*/
+            Console.WriteLine("1 + 2: " + plus.ToString());
 
             // Module 9, Task 1
-            //Console.WriteLine(Gcd.FindGcd());
+            Console.WriteLine(Gcd.FindGcd());
 
             // Module 9, Task 2
-            //Console.WriteLine(Module9.Task2.SortingMatrix.Sort(new int[,] { { 1, 2, 3 }, { 4, 0, 8 }, { 6, 5, 6 } }, false));
-            //Console.WriteLine(Module9.Task2.SortingMatrix.Sort(new int[,] { { 1, 2, 3 }, { 4, 0, 8 }, { 6, 15, 6 } }, true));
+            Console.WriteLine(Module9.Task2.SortingMatrix.Sort(new int[,] { { 1, 2, 3 }, { 4, 0, 8 }, { 6, 5, 6 } }, false));
+            Console.WriteLine(Module9.Task2.SortingMatrix.Sort(new int[,] { { 1, 2, 3 }, { 4, 0, 8 }, { 6, 15, 6 } }, true));
 
             //Module 9, Task 3
-            //ExampleRun.Task3Run();
+            ExampleRun.Task3Run();
 
             //Module 10, Task 2
-            //WorkWithFile.CountWordsInText();
+            WorkWithFile.CountWordsInText();
 
             //Module 2, Task 6
             /*int[] arr1 = new int[1_000_000_000];
